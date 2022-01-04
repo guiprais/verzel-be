28de132d43c240996316ab517be0e3a04a6302b7
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-04 17:04:27 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { classRouter, moduleRouter } from './routes';
 
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(classRouter);
