0cd555198f830e32f70116a981f0e786a6cf69b3
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-04 09:08:58 -0300

    feat: create controllers and repository files

---
+++ src/controllers/ClassController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+const index = async (request: Request, response: Response) => {
+  response.send('index route');
+};
+
+const store = async (request: Request, response: Response) => {
+  response.send('store route');
+};
+
+const update = async (request: Request, response: Response) => {
+  response.send('update route');
+};
+
+const remove = async (request: Request, response: Response) => {
+  response.send('remove route');
+};
+
+export {
+  index, store, update, remove,
+};
